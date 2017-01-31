3c08be93ec8bc59130b97120a0bd6b9e5fec6ac9
Aceeri <conmcclusk@gmail.com>
2017-01-30 20:20:49 -0800

    Parsing headers

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,17 +1,18 @@
 
 extern crate mptree;
 
-use mptree::decoder;
-use std::io::{Cursor};
+use mptree::{decoder, header};
+use std::io::{Read, Cursor};
+use std::fs::{File};
 
 fn main() {
-    let data: &[u8] = &[
-        0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00,
-        0xFF, 0xEF, 0xF2, 0x11, 0x05,
-        0x10, 0x50, 0x08, 0xA8, 0xC3,
-    ];
-    let cursor = Cursor::new(data);
-    let mut frame_reader = decoder::FrameReader::new(cursor);
-    println!("{:?}", frame_reader.read_until_header());
+    let file = File::open("examples/whatislove.mp3").unwrap();
+    let mut frame_reader = decoder::FrameReader::new(file);
+
+    for _ in 0..50 {
+        let header_data = frame_reader.read_until_header().unwrap();
+        println!("{:?}", header_data);
+        let header = header::Header::construct(&header_data);
+        println!("{:?}", header);
+    }
 }
--- src/header.rs
+++ src/header.rs
@@ -45,45 +45,24 @@ pub static SAMPLING_RATE: [[u16; 3]; 3] = [
     [11025, 12000, 8000],
 ];
 
-// Returns whether the value was free in the lookup table and what the bitrate was if it exists.
-pub fn bitrate(bit: u8, version: &Version, layer: &Layer, mode: &ChannelMode) -> Result<u16, MpError> {
-    if version == &Version::Reserved || layer == &Layer::Reserved {
-        return Err(MpError::Reserved);
-    }
-
-    let column_index = match (version, layer) {
-        (&Version::Version1, &Layer::Layer1) => 0,
-        (&Version::Version1, &Layer::Layer2) => 1,
-        (&Version::Version1, &Layer::Layer3) => 2,
-        (_, &Layer::Layer1) => 3,
-        (_, _) => 4,
-    };
-
-    if bit > 15 {
-        return Err(MpError::BadBit(bit as u16)); // bit index was too high
-    }
-
-    let bitrate = BITRATE_INDEX[column_index][bit as usize];
-    if layer == &Layer::Layer2 {
-        // if the bitrate is 32, 48, 56, or 80 and the channel is not mono, then invalid
-        if ((bitrate >= 32 && bitrate <= 56) || (bitrate == 80)) && mode != &ChannelMode::Mono {
-            return Err(MpError::InvalidMode(mode.clone(), vec![ChannelMode::Mono]));
-        }
-
-        if bitrate >= 224 && mode == &ChannelMode::Mono {
-            return Err(MpError::InvalidMode(mode.clone(), vec![ChannelMode::Dual, ChannelMode::Stereo, ChannelMode::JointStereo]));
-        }
-    }
-
-    Ok(bitrate)
-}
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum Version {
-    Reserved,
     Version2_5, // unofficial version for very low bitrate files
+    Reserved,
     Version2, // MPEG Version 2 (ISO/IEC 13818-3)
     Version1, // MPEG Version 1 (ISO/IEC 11172-3)
+}
+
+impl From<u8> for Version {
+    fn from(data: u8) -> Version {
+        match data {
+            0 => Version::Version2_5,
+            1 => Version::Reserved,
+            2 => Version::Version2,
+            3 => Version::Version1,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -94,22 +73,163 @@ pub enum Layer {
     Layer1,
 }
 
+impl From<u8> for Layer {
+    fn from(data: u8) -> Layer {
+        match data {
+            0 => Layer::Reserved,
+            1 => Layer::Layer3,
+            2 => Layer::Layer2,
+            3 => Layer::Layer1,
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum ChannelMode {
     Stereo,
-    JointStereo,
+    JointStereo(Extension),
     Dual, // 2 mono
     Mono,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Extension {
+    Bands(u8), // Bands (Minimum -> 31)
+    Stereo(bool, bool), // Intensity stereo, MS stereo
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Header {
     version: Version, // Version 1-2 (along with the unofficial 2.5)
     layer: Layer, // Layer 1-3
     protection: bool, // Protected by a 16 bit CRC following header
     bitrate: u16, // Bitrate (kbps)
     sampling_rate: u16, // Sampling rate frequency index (Hz)
-    padding: bool, // Frame padding for one extra slot
+    padding: u8, // Frame padding for one extra slot
     private: bool, // Freely used for applications
+    copyright: bool, // Is copyrighted
+    original: bool, // Is the original copy
     channel: ChannelMode, // Mono, Dual, Stereo, JointStereo
-    channel_ext: Option<(bool, bool)>, // Intensity stereo / MS stereo 
 }
+
+impl Header {
+    pub fn construct(data: &[u8; 4]) -> Result<Header, MpError> {
+        let version = ((data[1] & 0b0001_1000) >> 3).into();
+        let layer: Layer = ((data[1] & 0b0000_0110) >> 1).into();
+        let protection: bool = (data[1] & 0b0000_0001) == 1;
+        let bitrate_index: u8 = (data[2] & 0b1111_0000) >> 4;
+        let sampling_index: u8 = (data[2] & 0b0000_1100) >> 2;
+        let padding: u8 = data[2] & 0b0000_0010;
+        let private: bool = (data[2] & 0b0000_0001) == 1;
+        let channel = match (data[3] & 0b1100_0000) >> 6 {
+            0 => ChannelMode::Stereo,
+            1 => {
+                let extension = data[3] & 0b0011_0000; // Joint Stereo extension
+
+                let parsed_extension = match layer {
+                    Layer::Layer1 | Layer::Layer2 => {
+                        let value = 4 + (4 * extension >> 4);
+                        Extension::Bands(value)
+                    },
+                    Layer::Layer3 => {
+                        let intensity = extension >> 5 == 1;
+                        let ms = extension >> 4 & 1 == 1;
+                        Extension::Stereo(intensity, ms)
+                    },
+                    _ => return Err(MpError::Reserved),
+                };
+                ChannelMode::JointStereo(parsed_extension)
+            },
+            2 => ChannelMode::Dual,
+            3 => ChannelMode::Mono,
+            _ => unreachable!(),
+        };
+        let copyright = (data[3] & 0b0000_1000) == 8;
+        let original = (data[3] & 0b0000_0100) == 4;
+        let emphasis = data[3] & 0b0000_0011;
+
+        let bitrate = try!(Header::bitrate(bitrate_index, &version, &layer, &channel));
+        let sampling_rate = try!(Header::sampling_rate(sampling_index, &version));
+
+        Ok(Header {
+            version: version,
+            layer: layer,
+            protection: protection,
+            bitrate: bitrate,
+            sampling_rate: sampling_rate,
+            padding: padding,
+            private: private,
+            copyright: copyright,
+            original: original,
+            channel: channel,
+        })
+    }
+
+    // Returns the bitrate of the header.
+    pub fn bitrate(bit: u8, version: &Version, layer: &Layer, channel: &ChannelMode) -> Result<u16, MpError> {
+        if version == &Version::Reserved || layer == &Layer::Reserved {
+            return Err(MpError::Reserved);
+        }
+
+        let column_index = match (version, layer) {
+            (&Version::Version1, &Layer::Layer1) => 0,
+            (&Version::Version1, &Layer::Layer2) => 1,
+            (&Version::Version1, &Layer::Layer3) => 2,
+            (_, &Layer::Layer1) => 3,
+            (_, _) => 4,
+        };
+
+        if bit > 15 {
+            return Err(MpError::BadBit(bit as u16)); // bit index was too high
+        }
+
+        let bitrate = BITRATE_INDEX[column_index][bit as usize];
+        if layer == &Layer::Layer2 {
+            // if the bitrate is 32, 48, 56, or 80 and the channel is not mono, then invalid
+            if ((bitrate >= 32 && bitrate <= 56) || (bitrate == 80)) && channel != &ChannelMode::Mono {
+                return Err(MpError::InvalidMode(channel.clone(), vec![ChannelMode::Mono]));
+            }
+            
+            if bitrate >= 224 {
+                match channel {
+                    &ChannelMode::JointStereo(Extension::Stereo(false, _)) |
+                    &ChannelMode::Mono => {
+                        return Err(
+                            MpError::InvalidMode(
+                                channel.clone(), 
+                                vec![
+                                    ChannelMode::Dual,
+                                    ChannelMode::Stereo,
+                                    ChannelMode::JointStereo(Extension::Stereo(true, false)),
+                                    ChannelMode::JointStereo(Extension::Stereo(true, true)),
+                                ],
+                            )
+                        );
+                    },
+                    _ => (),
+
+                }
+            }
+        }
+
+        Ok(bitrate)
+    }
+
+    // Returns the sampling rate from the version and the sampling index. Errors if takes in reserved values.
+    pub fn sampling_rate(bit: u8, version: &Version) -> Result<u16, MpError> {
+        if bit == 3 {
+            Err(MpError::Reserved)
+        }
+        else {
+            let index = match version {
+                &Version::Version1 => 0,
+                &Version::Version2 => 1,
+                &Version::Version2_5 => 2,
+                _ => return Err(MpError::Reserved),
+            };
+            Ok(SAMPLING_RATE[index][bit as usize])
+        }
+    }
+}
+
